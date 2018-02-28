ae9959bed15bcaf4473139e2f2fab14bcfab9da4
joechesney <33613119+joechesney@users.noreply.github.com>
2018-02-28 14:24:52 -0600

    finished

---
+++ helloworld.js
@@ -0,0 +1,7 @@
+const helloWorld = "hello world";
+console.log(helloWorld);
+process.stdout.write(`Waddup! We're 3 cool dudes! ${helloWorld}\n`);
+process.stdout.write(`hello world`);
+
+
+// I did it!
